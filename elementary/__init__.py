072e4447f0a8b3b8846e4625ed40b6bb01558c4c
i-a-morozov <i-a-morozov@users.noreply.github.com>
2025-06-19 23:46:56 +0700

    06_19_2025: initial commit

---
+++ elementary/__init__.py
@@ -0,0 +1,31 @@
+"""
+Version and aliases
+
+"""
+__version__ = '0.0.1'
+
+__all__ = [
+    'nest',
+    'nest_list',
+    'fold',
+    'fold_list',
+    'weights',
+    'coefficients',
+    'table',
+    'sequence',
+    'midpoint',
+    'tao'
+]
+
+from sympint.functional import nest
+from sympint.functional import nest_list
+from sympint.functional import fold
+from sympint.functional import fold_list
+
+from sympint.yoshida import weights
+from sympint.yoshida import coefficients
+from sympint.yoshida import table
+from sympint.yoshida import sequence
+
+from sympint.integrators import midpoint
+from sympint.integrators import tao
---
+++ elementary/hamiltonian.py
@@ -0,0 +1,89 @@
+"""
+Hamiltonian
+-----------
+
+Generic single particle Hamiltonian factory
+
+"""
+from typing import Callable
+from typing import Optional
+
+import jax
+from jax import Array
+
+
+def hamiltonian_factory(vector:Callable[..., tuple[Array, Array, Array]],
+                        scalar:Callable[..., Array], *,
+                        curvature:Optional[Callable[..., Array]]=None,
+                        torsion:Optional[Callable[..., Array]]=None,
+                        beta:Optional[float]=None,
+                        gamma:Optional[float]=None) -> Callable[..., Array]:
+    """
+    Generic single particle Hamiltonian factory
+
+    H = H(qs, ps; s) = H(q_x, q_y, q_s, p_x, p_y, p_s; s)
+    H = p_s/beta - torsion(s)*(q_x*p_y - q_y*p_x) - (1 + curvature(s)*q_x)*(root + a_s(qs, s))
+    root = sqrt(P_s**2 - P_x**2 - P_y**2 - 1/(beta*gamma)**2)
+    P_s = p_s + 1/beta - scalar(qs, s)
+    P_x = p_x - a_x(qs, s)
+    P_y = p_y - a_y(qs, s)
+
+    Parameters
+    ----------
+    vector: Callable[..., tuple[Array, Array, Array]]
+        normalized vector potential
+    scalar: Callable[..., Array]
+        normalized scalar potential
+    curvature: Optional[Callable[..., Array]]
+        curvature
+    torsion: Optional[Callable[..., Array]]
+        torsion
+    beta: Optional[float]
+        beta
+    gamma: Optional[float]
+        gamma
+
+    Returns
+    -------
+    Callable[..., Array]
+
+    Note
+    ----
+    Vector and scalar potentials are assumed to have (qs, *args) signatures
+    Curvature and torsion are functions of independent parameter
+    The resulting hamiltonian has (qs, ps, s, *args) signature
+
+    """
+    beta = beta if beta else 1.0
+    constant = 1/(beta**2*gamma**2) if gamma else 0.0
+    if curvature and torsion:
+        def hamiltonian(qs: Array, ps: Array, s: Array, *args: Array) -> Array:
+            q_x, q_y, *_ = qs
+            p_x, p_y, p_s = ps
+            a_x, a_y, a_s = vector(qs, s, *args)
+            P_s = p_s + 1/beta - scalar(qs, s, *args)
+            P_x = p_x - a_x
+            P_y = p_y - a_y
+            root = jax.numpy.sqrt(P_s**2 - P_x**2 - P_y**2 - constant)
+            return p_s/beta - torsion(s)*(q_x*p_y - q_y*p_x) - (1 + curvature(s)*q_x)*(root + a_s)
+        return hamiltonian
+    if curvature:
+        def hamiltonian(qs: Array, ps: Array, s: Array, *args: Array) -> Array:
+            q_x, *_ = qs
+            p_x, p_y, p_s = ps
+            a_x, a_y, a_s = vector(qs, s, *args)
+            P_s = p_s + 1/beta - scalar(qs, s, *args)
+            P_x = p_x - a_x
+            P_y = p_y - a_y
+            root = jax.numpy.sqrt(P_s**2 - P_x**2 - P_y**2 - constant)
+            return p_s/beta - (1 + curvature(s)*q_x)*(root + a_s)
+        return hamiltonian
+    def hamiltonian(qs: Array, ps: Array, s: Array, *args: Array) -> Array:
+        p_x, p_y, p_s = ps
+        a_x, a_y, a_s = vector(qs, s, *args)
+        P_s = p_s + 1/beta - scalar(qs, s, *args)
+        P_x = p_x - a_x
+        P_y = p_y - a_y
+        root = jax.numpy.sqrt(P_s**2 - P_x**2 - P_y**2 - constant)
+        return p_s/beta - (root + a_s)
+    return hamiltonian
