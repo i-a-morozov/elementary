223d5994b3fae1c652c2d5546c885b0acf958afd
i-a-morozov <i-a-morozov@users.noreply.github.com>
2025-06-23 12:53:27 +0700

    06_23_2025: added straight multipole (mad-x kn & ks)

---
+++ elementary/multipole.py
@@ -0,0 +1,78 @@
+"""
+Multipole
+---------
+
+Multipole element factory
+
+"""
+from typing import Optional
+from typing import Callable
+
+import jax
+from jax import Array
+
+from elementary.element import element_factory
+
+def vector(qs:Array,
+           s:Array,
+           kq_n:Array,
+           kq_s:Array,
+           ks_n:Array,
+           ks_s:Array,
+           ko_n:Array,
+           ko_s:Array) -> tuple[Array, Array, Array]:
+    """
+    Vector potential
+
+    """
+    q_x, q_y, _ = qs
+    a_x, a_y, a_s = jax.numpy.zeros_like(qs)
+    a_s = a_s - 1/2*kq_n*(q_x**2 - q_y**2) + kq_s*q_x*q_y
+    a_s = a_s - ks_n/2*(q_x**3/3 - q_x*q_y**2) - ks_s/2*(-q_x**2*q_y + q_y**3/3)
+    a_s = a_s - ko_n/6*(q_x**4/4 - 3*q_x**2*q_y**2/2 + q_y**4/4) - ko_s/6*(-q_x**3*q_y + q_x*q_y**3)
+    return a_x, a_y, a_s
+
+
+def multipole_factory(beta:Optional[float]=None,
+                      gamma:Optional[float]=None,
+                      driver:Optional[Callable[..., Array]]=None,
+                      settings:Optional[dict]=None,
+                      order:int=0,
+                      iterations:int=1) -> Callable[..., Array]:
+    """
+    Multipole element transfer map
+
+    Parameters
+    ----------
+    beta: Optional[float]
+        relativistic beta
+    gamma: Optional[float]
+        relativistic gamma
+    driver: Optional[Callable[..., Array]]
+        symplectic integrator (midpoint)
+    settings: Optional[dict]
+        configuration settings for integrator
+    order: int, default=0
+        yoshida composition order
+    iterations: int, default=1
+        number of integration
+    autonomous: bool, default=True
+        autonomous flag
+
+    Returns
+    -------
+    Callable[..., Array]
+
+    """
+    element = element_factory(vector,
+                              scalar=None,
+                              beta=beta,
+                              gamma=gamma,
+                              driver=driver,
+                              settings=settings,
+                              order=order,
+                              iterations=iterations,
+                              autonomous=True)
+    def multipole(qsps, length, kq_n, kq_s, ks_n, ks_s, ko_n, ko_s):
+        return element(qsps, length, 0.0, kq_n, kq_s, ks_n, ks_s, ko_n, ko_s)
+    return multipole
