b33cf5dfaf9cbc744e117575c8f4f06e10dfe091
i-a-morozov <i-a-morozov@users.noreply.github.com>
2025-06-23 11:29:04 +0700

    06_23_2025: generic drift

--- elementary/drift.py
+++ elementary/drift.py
@@ -8,72 +8,12 @@ Drift element factory
 from typing import Optional
 from typing import Callable
 
-from functools import partial
-
 import jax
 from jax import Array
 
-from elementary import fold
-from elementary import nest
-from elementary import midpoint
-from elementary import sequence
+from elementary.element import element_factory
 
-from elementary.hamiltonian import hamiltonian_factory
-
-def drift(length:Optional[Array]=None,
-          beta:Optional[float]=None,
-          gamma:Optional[float]=None,
-          integrator:Optional[Callable[..., Array]]=None,
-          settings:Optional[dict]=None,
-          order:int=0,
-          iterations:int=1) -> Callable[..., Array]:
-    """
-    Generate drift element transfer map
-
-    Parameters
-    ----------
-    length: Optional[Array]
-        length (parametric)
-    beta: Optional[float]
-        relativistic beta
-    gamma: Optional[float]
-        relativistic gamma
-    integrator: Optional[Callable[..., Array]]
-        symplectic integrator (midpoint)
-    settings: Optional[dict]
-        configuration settings for integrator
-    order: int, default=0
-        yoshida composition order
-    iterations: int, default=1
-        number of integration
-
-    Returns
-    -------
-    Callable[..., Array]
-
-    Note
-    ----
-    If length is not provided on construction, it is expected to be passed on call
-    In this case, element transfer map is differentiable with respect to such parameter(s)
-
-    If element parameters are specified on initializatin
-    The returned element has 'rc' attribute containing construction parameters
-
-    """
-    hamiltonian = hamiltonian_factory(vector, scalar=None, beta=beta, gamma=gamma)
-    data = [(integrator if integrator else midpoint)(hamiltonian, **settings if settings else {})]
-    step = fold(sequence(0, order, data, merge=False))
-    def closure(qsps, length=None):
-        return nest(iterations, step)(qsps, length/iterations, 0.0)
-    kwargs = {}
-    if length:
-        kwargs['length'] = length
-    element = partial(closure, **kwargs)
-    element.rc = kwargs
-    return element
-
-
-def vector(qs:Array, s:Array, *args:Array) -> Array:
+def vector(qs:Array, s:Array) -> tuple[Array, Array, Array]:
     """
     Vector potential
 
@@ -81,9 +21,46 @@ def vector(qs:Array, s:Array, *args:Array) -> Array:
     return tuple(jax.numpy.zeros_like(qs))
 
 
-def scalar(qs:Array, s:Array, *args:Array) -> Array:
+def drift_factory(beta:Optional[float]=None,
+                  gamma:Optional[float]=None,
+                  driver:Optional[Callable[..., Array]]=None,
+                  settings:Optional[dict]=None,
+                  order:int=0,
+                  iterations:int=1) -> Callable[..., Array]:
     """
-    Scalar potential
+    Drift element transfer map
+
+    Parameters
+    ----------
+    beta: Optional[float]
+        relativistic beta
+    gamma: Optional[float]
+        relativistic gamma
+    driver: Optional[Callable[..., Array]]
+        symplectic integrator (midpoint)
+    settings: Optional[dict]
+        configuration settings for integrator
+    order: int, default=0
+        yoshida composition order
+    iterations: int, default=1
+        number of integration
+    autonomous: bool, default=True
+        autonomous flag
+
+    Returns
+    -------
+    Callable[..., Array]
 
     """
-    return jax.numpy.zeros_like(s)
+    element = element_factory(vector,
+                              scalar=None,
+                              beta=beta,
+                              gamma=gamma,
+                              driver=driver,
+                              settings=settings,
+                              order=order,
+                              iterations=iterations,
+                              autonomous=True)
+    def drift(qsps, length):
+        return element(qsps, length, 0.0)
+    return drift
