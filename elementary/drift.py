3f70c5b9912863dc141086db666015310fe9566d
i-a-morozov <i-a-morozov@users.noreply.github.com>
2025-06-24 13:34:08 +0700

    06_24_2025: added exact dipole transformation

--- elementary/drift.py
+++ elementary/drift.py
@@ -14,7 +14,8 @@ from jax import Array
 from elementary.element import element_factory
 
 
-def drift_factory(beta:Optional[float]=None,
+def drift_factory(exact:bool=False,
+                  beta:Optional[float]=None,
                   gamma:Optional[float]=None,
                   driver:Optional[Callable[..., Array]]=None,
                   settings:Optional[dict]=None,
@@ -26,6 +27,8 @@ def drift_factory(beta:Optional[float]=None,
 
     Parameters
     ----------
+    exact: bool, default=False
+        ideal transfromation
     beta: Optional[float]
         relativistic beta
     gamma: Optional[float]
@@ -46,6 +49,12 @@ def drift_factory(beta:Optional[float]=None,
     Callable[..., Array]
 
     """
+    if exact:
+        beta = beta if beta else 1.0
+        constant = 1/(beta**2*gamma**2) if gamma else 0.0
+        def drift(qsps:Array, length:Array) -> Array:
+            return mapping(qsps, length, beta, constant)
+        return drift
     element = element_factory(vector,
                               scalar=None,
                               beta=beta,
@@ -66,3 +75,19 @@ def vector(qs:Array, s:Array) -> tuple[Array, Array, Array]:
 
     """
     return tuple(jax.numpy.zeros_like(qs))
+
+
+def mapping(qsps:Array, dz:Array, beta:float=1.0, constant:float=0.0) -> Array:
+    """
+    Exact sector bend body transformation
+
+    """
+    q_x, q_y, q_s, p_x, p_y, p_s = qsps
+    dp = jax.numpy.sqrt((1/beta + p_s)**2 - p_x**2 - p_y**2 - constant)
+    Q_x = q_x + p_x*dz/dp
+    Q_y = q_y + p_y*dz/dp
+    Q_s = q_s + dz/beta - dz*(1/beta + p_s)/dp
+    P_x = p_x
+    P_y = p_y
+    P_s = p_s
+    return jax.numpy.stack([Q_x, Q_y, Q_s, P_x, P_y, P_s])
